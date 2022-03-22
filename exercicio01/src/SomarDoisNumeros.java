dd77c1b286b6222c16a5ff4f83ef8a0331ebb5b1
edmar <edmar@Lexizz>
2022-03-22 12:42:10 -0300

    Added SomarDoisNumeros class

---
+++ exercicio01/src/SomarDoisNumeros.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class SomarDoisNumeros {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int num1, num2, soma;
+		
+		System.out.println("Digite um numero");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro numero");
+		num2 = sc.nextInt();
+		
+		soma = num1 + num2;
+		
+		System.out.println("Soma: " + soma);
+	}
+
+}
